826f56af741210f6ca5c02be475d6a3545afb2e8
unknown <racheli.bruk@gmail.com>
2014-12-30 23:32:09 +0200

    fix image angular

--- angular/cars.js
+++ angular/cars.js
@@ -1,21 +1,6 @@
 var app = angular.module('myApp', []);
 
 
-
-app.filter("geturl",function(){
-	return function(car){
-		return "try";
-	}
-})
-
-// angular.module('phonecatFilters', []).filter('checkmark', function() {
-//   return function(input) {
-//     return input ? '\u2713' : '\u2718';
-//   };
-// });
-
-
-
 function getCars($scope,$http) {
     $http.get("http://linnotest.localhost:8082/cars")
     .success(function(response) {$scope.cars = response;});
